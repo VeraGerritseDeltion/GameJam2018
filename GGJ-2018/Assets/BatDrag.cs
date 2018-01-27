cbf0cc0574a9aa4776bcf5a6c1c5da142240c62c
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-01-27 12:20:51 +0100

    Tasteful motionblur, downward tilt

--- GGJ-2018/Assets/BatDrag.cs
+++ GGJ-2018/Assets/BatDrag.cs
@@ -6,6 +6,12 @@ public class BatDrag : MonoBehaviour{
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        this.gameObject.transform.rotation = Quaternion.Euler(0, 0, x *-10);
+        float y = Input.GetAxis("Vertical");
+
+        if(y >= 0.1f)
+        {
+            y = 0;
+        }
+        this.gameObject.transform.rotation = Quaternion.Euler(y*40, 0, x *-10);
     }
 }
