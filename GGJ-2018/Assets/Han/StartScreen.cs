a79531be62c9b335f4facca43357c19766cbcdfd
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-01-27 01:53:00 +0100

    han

---
+++ GGJ-2018/Assets/Han/StartScreen.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour{
+    public GameObject startCanvas,pauseCanvas;
+    public GameObject quitMenu, optionMenu;
+
+    void Start(){
+        quitMenu.SetActive(false);
+        pauseCanvas.SetActive(false);
+        optionMenu.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    private void Update(){
+        if (Input.GetButtonDown("Cancel")){
+            if(pauseCanvas.activeSelf == false && startCanvas.activeSelf == false){
+                pauseCanvas.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else if(quitMenu.activeSelf == true || optionMenu.activeSelf == true){
+                quitMenu.SetActive(false);
+                optionMenu.SetActive(false);
+            }
+            else{
+                pauseCanvas.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+
+    public void StartGame(){
+        startCanvas.SetActive(false);
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void EndGame(){
+        quitMenu.SetActive(true);
+    }
+
+    public void OptionMenu(){
+        optionMenu.SetActive(true);
+    }
+
+    public void EndGameYes(){
+        Application.Quit();
+    }
+
+    public void EndGameNo(){
+        quitMenu.SetActive(false);
+    }
+}
