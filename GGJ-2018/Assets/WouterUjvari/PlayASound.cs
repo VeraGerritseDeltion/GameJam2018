bd3b716d29ba7985e0fbaacb40bd4f4fb237897b
Dominik Beens <dominik.beens@hotmail.nl>
2018-01-27 19:26:55 +0100

    test

---
+++ GGJ-2018/Assets/WouterUjvari/PlayASound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayASound : MonoBehaviour {
+    public AudioSource myAudioSource;
+
+
+    public void PlayASoundForMe(float pitch)
+    {
+        
+        myAudioSource.pitch = pitch;
+        myAudioSource.Play();
+    }
+}
