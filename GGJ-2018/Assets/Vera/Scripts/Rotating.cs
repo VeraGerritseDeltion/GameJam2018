01f8f43839976954f4d8ff562696b4e8527bab24
VeraGerritseDeltion <arev_97@live.nl>
2018-01-27 13:19:26 +0100

    propellor

---
+++ GGJ-2018/Assets/Vera/Scripts/Rotating.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public float speed;
+	void Update () {
+        transform.Rotate(transform.forward * speed * Time.deltaTime / 10 );
+	}
+}
