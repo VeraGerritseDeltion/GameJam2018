7b38325296afabeaf1c649fad5c6c7222bca6892
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-01-27 01:58:43 +0100

    Sniffing and tilt

---
+++ GGJ-2018/Assets/BatDrag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatDrag : MonoBehaviour{
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        this.gameObject.transform.rotation = Quaternion.Euler(0, 0, x *-10);
+    }
+}
---
+++ GGJ-2018/Assets/GiveRandom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveRandom : MonoBehaviour {
+
+    public float rate;
+    public float timer;
+    public float chance;
+    public Animator anim;
+    public int test;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        test = Random.Range(0, 10);
+        timer += Time.deltaTime;
+        if(timer >= rate)
+        {
+            timer = 0;
+            Randomize();
+        }
+    }
+
+    void Randomize()
+    {
+        anim.SetInteger("Numbah", test);
+    }
+}
