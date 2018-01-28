80e8424ad1ad9c5927a9d8b2fddfda56839fcadd
HanHakkenbergDeltion <han.hakkenberg@gmail.com>
2018-01-28 03:22:35 +0100

    l

---
+++ GGJ-2018/Assets/Han/BossBattleManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBattleManager : MonoBehaviour {
+    public static BossBattleManager bsm;
+    public Transform target;
+}
---
+++ GGJ-2018/Assets/Han/Spider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : MonoBehaviour {
+    public float spiderSpeed;
+    public BossBattleManager bsm;
+
+    private void Update(){
+        
+
+        Vector3 offset = new Vector3(bsm.target.position.x - transform.localPosition.x, bsm.target.position.y - transform.localPosition.y,0);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle + 90);
+
+        transform.Translate(-Vector3.up * Time.deltaTime * spiderSpeed);
+    }
+
+    public void Movement(){
+
+    }
+}
